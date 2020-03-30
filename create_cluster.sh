6b60ff90f26c647d93d88f28d0ea4fe193d87334
Asmir Jahic <asmirjahic@gmail.com>
2020-03-30 21:05:41 +0200

    Fixing create_cluster.sh file inside development

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
+# eksctl create cluster \
+#   --version 1.15 \
+#   --region us-east-2 \
+#   --node-type t3.micro \
+#   --nodes 2 \
+#   --nodes-min 1 \
+#   --nodes-max 3 \
+#   --name capstone-project
+
 eksctl create cluster \
   --version 1.15 \
   --region us-east-2 \
-  --node-type t3.micro \
-  --nodes 2 \
-  --nodes-min 1 \
-  --nodes-max 3 \
-  --name capstone-project
+  --name=capstone-project \
+  --ssh-access=true \
+  --ssh-public-key=sreejenkins_key \
+  --nodegroup-name=standard-workers \
+  --node-type=t3.micro \
+  --nodes=3 \
+  --nodes-min=1 \
+  --nodes-max=4 
+  --node-ami=auto
