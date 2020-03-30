2bf51258c972ff7de9bd01fc346faf95db73d0f9
Asmir Jahic <asmirjahic@gmail.com>
2020-03-30 20:47:04 +0200

    Fixing create_cluster.sh file inside development

--- create_cluster.sh
+++ create_cluster.sh
@@ -4,7 +4,7 @@ eksctl create cluster \
   --version 1.15 \
   --region us-east-2 \
   --node-type t3.micro \
-  --nodes 3 \
+  --nodes 2 \
   --nodes-min 1 \
-  --nodes-max 2 \
+  --nodes-max 3 \
   --name capstone-project
