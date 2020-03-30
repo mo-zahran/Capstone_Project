98c1703ecc92571ce8971bad8d04aabf4f9c2424
Asmir Jahic <asmirjahic@gmail.com>
2020-03-30 22:18:26 +0200

    Modifying run_kubernetes.sh inside development

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,12 +2,12 @@
 
 image="asmirjahic/capstone:latest"
 
-kubectl create deployment capstone --image=$image
+/home/ubuntu/bin/kubectl create deployment capstone --image=$image
 
 sleep 20
 
-kubectl get pods --all-namespaces
+/home/ubuntu/bin/kubectl get pods --all-namespaces
 
-kubectl expose deployment capstone --type="NodePort" --port=80
+/home/ubuntu/bin/kubectl expose deployment capstone --type="NodePort" --port=80
 
-kubectl port-forward deployment/capstonr 8081:80
+/home/ubuntu/bin/kubectl port-forward deployment/capstonr 8081:80
