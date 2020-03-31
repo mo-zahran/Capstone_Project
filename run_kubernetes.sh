9ea9197007e159297e577a424c190b764198385d
Asmir Jahic <asmirjahic@gmail.com>
2020-03-31 18:48:06 +0200

    Modifying run_kubernetes.sh inside development

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-/usr/local/bin/aws eks --region us-east-2 update-kubeconfig --name capstone-cluste
+aws eks --region us-east-2 update-kubeconfig --name capstone-cluster
 # /home/ubuntu/bin/kubectl apply -f deployment.yaml
 # /home/ubuntu/bin/kubectl apply -f service.yaml 
