de16c5588e473a4eaf21de8129fbec8506b6d4c8
Asmir Jahic <asmirjahic@gmail.com>
2020-03-31 16:49:38 +0200

    Modifying run_kubernetes.sh inside development

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-
-/home/ubuntu/bin/kubectl apply -f deployment.yaml
-/home/ubuntu/bin/kubectl apply -f service.yaml 
+/home/ubuntu/bin/kubectl api-resources
+#/home/ubuntu/bin/kubectl apply -f deployment.yaml
+#/home/ubuntu/bin/kubectl apply -f service.yaml 
